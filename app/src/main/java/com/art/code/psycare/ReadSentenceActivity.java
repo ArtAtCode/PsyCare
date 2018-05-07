e18c90111a4e6f0dd05953811bcf4a73b18724cf
Lmj <1594485527@qq.com>
2018-05-07 14:00:49 +0800

    banner/录音（有bug）

--- app/src/main/java/com/art/code/psycare/ReadSentenceActivity.java
+++ app/src/main/java/com/art/code/psycare/ReadSentenceActivity.java
@@ -1,5 +1,9 @@
 package com.art.code.psycare;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,9 +19,44 @@ public class ReadSentenceActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if(actionBar!=null) actionBar.hide();
         TextView textView = findViewById(R.id.sentence_read_text);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+                ){
+            ActivityCompat.requestPermissions(ReadSentenceActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+
+        }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(ReadSentenceActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},2);
+        }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(ReadSentenceActivity.this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},3);
+        }
         setContentView(R.layout.activity_read_sentence);
     }
-
-
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+        switch(requestCode){
+            case 1:
+                if(grantResults.length>0&&grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+                    this.finish();
+                    break;
+                }
+            case 2:
+                if(grantResults.length>0&&grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+                    this.finish();
+                    break;
+                }
+            case 3:
+                if(grantResults.length>0&&grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+                this.finish();
+                break;
+            }
+        }
+    }
 }
