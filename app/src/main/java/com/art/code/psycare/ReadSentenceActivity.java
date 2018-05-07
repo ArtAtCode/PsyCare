0966745d52ddc3d307d5eb6cd37b11fe504f6ebb
Aneon <2016141463174@stu.scu.edu.cn>
2018-05-07 20:42:29 +0800

    修复录音崩溃的bug并改了部分Warning

--- app/src/main/java/com/art/code/psycare/ReadSentenceActivity.java
+++ app/src/main/java/com/art/code/psycare/ReadSentenceActivity.java
@@ -2,24 +2,44 @@ package com.art.code.psycare;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextPaint;
-import android.util.TypedValue;
-import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReadSentenceActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_OF_RECORD_AUDIO = 1;
+
+    private String[] mPermissions = new String[] {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO
+    };
+
+    List<String> mPermissionList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActionBar actionBar = getSupportActionBar();
-        if(actionBar!=null) actionBar.hide();
-        TextView textView = findViewById(R.id.sentence_read_text);
 
+
+
+        mPermissionList.clear();
+        for (String mPermission : mPermissions) {
+            if (ContextCompat.checkSelfPermission(this, mPermission) != PackageManager.PERMISSION_GRANTED)
+                mPermissionList.add(mPermission);
+        }
+        if (!mPermissionList.isEmpty()) {
+            String[] permissions = mPermissionList.toArray(new String[mPermissionList.size()]);
+            ActivityCompat.requestPermissions(this, permissions, PERMISSION_OF_RECORD_AUDIO);
+        }
+
+        /*
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED
                 ){
@@ -37,16 +57,18 @@ public class ReadSentenceActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(ReadSentenceActivity.this,
                     new String[]{Manifest.permission.RECORD_AUDIO},3);
         }
+        */
         setContentView(R.layout.activity_read_sentence);
     }
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults){
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
         switch(requestCode){
-            case 1:
+            case PERMISSION_OF_RECORD_AUDIO:
                 if(grantResults.length>0&&grantResults[0]!=PackageManager.PERMISSION_GRANTED){
                     this.finish();
                     break;
                 }
+                /*
             case 2:
                 if(grantResults.length>0&&grantResults[0]!=PackageManager.PERMISSION_GRANTED){
                     this.finish();
@@ -56,7 +78,9 @@ public class ReadSentenceActivity extends AppCompatActivity {
                 if(grantResults.length>0&&grantResults[0]!=PackageManager.PERMISSION_GRANTED){
                 this.finish();
                 break;
-            }
+                */
+            default:
+               break;
         }
     }
 }
